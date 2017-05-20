442f8994a7f68768430640bebf1bc5563dff81ff
Mark Sullivan <marksullivan.sp@gmail.com>
2017-05-20 06:51:25 -0400

    initial solution setup

---
+++ TrainingManagement/TrainingManagement/Global.asax.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web;
+
+namespace TrainingManagement
+{
+    public class Global : HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
